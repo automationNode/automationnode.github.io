d269a72c964b3a36438693fe71c7d5e08824e17b
automationNode <84304854+automationNode@users.noreply.github.com>
2021-05-20 22:38:52 -0600

    Add files via upload

---
+++ software/index.js
@@ -0,0 +1,10 @@
+var softwareWindows = document.getElementById("softwareWindows")
+var softwareLinux = document.getElementById("softwareLinux")
+
+softwareWindows.addEventListener("click", function(){
+    location.replace("https://drive.google.com/file/d/1a9x-kTivSpmVLeys84ErMIROWah9uxD6/view?usp=sharing")
+})
+
+softwareLinux.addEventListener("click", function(){
+    location.replace("")
+})
