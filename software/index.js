61d3b4bea69770d25975ac6c08246973a11882cc
automationNode <84304854+automationNode@users.noreply.github.com>
2021-05-23 20:55:40 -0600

    Add files via upload

--- software/index.js
+++ software/index.js
@@ -2,7 +2,7 @@ var softwareWindows = document.getElementById("softwareWindows")
 var softwareLinux = document.getElementById("softwareLinux")
 
 softwareWindows.addEventListener("click", function(){
-    location.replace("https://drive.google.com/file/d/1a9x-kTivSpmVLeys84ErMIROWah9uxD6/view?usp=sharing")
+    location.replace("https://drive.google.com/file/d/1pOhg-ms7PEu8B_AyPQD3OX7vADNfovCa/view?usp=sharing")
 })
 
 softwareLinux.addEventListener("click", function(){
