8aa852855798bc8c659bb74da4e4c3c7b5e216df
automationNode <84304854+automationNode@users.noreply.github.com>
2021-12-01 00:11:36 -0700

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,9 +1,13 @@
 var navbar = document.getElementById("navbar")
 
 navbar.innerHTML = `
-<div class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 1%;">
+<div class="navbar navbar-expand-lg navbar-secondary" style="padding: 1%; style="color: black;"">
 <a class="navbar-brand" href="https://automationnode.github.io/index.html">Automation Node</a>
 <ul class="navbar-nav mr-auto">
+    <li class="nav-item" style="margin-left:10%;">
+      <a class="nav-link" href="https://automationnode.github.io/products/plcNode/index.html" style="padding: 3%;">PlcNode</a>
+    </li>
+
     <li class="nav-item" style="margin-left:10%;">
       <a class="nav-link" href="https://automationnode.github.io/software/index.html" style="padding: 3%;">Software</a>
     </li>
