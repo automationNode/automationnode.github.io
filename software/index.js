e278976770c29b57556e95d2f49e6c57b62d5a3d
automationNode <84304854+automationNode@users.noreply.github.com>
2021-12-02 11:21:39 -0700

    Add files via upload

--- software/index.js
+++ software/index.js
@@ -1,5 +1,6 @@
 var softwareWindows = document.getElementById("softwareWindows")
 var softwareLinux = document.getElementById("softwareLinux")
+let getStartedBtn = document.getElementById("getStartedBtn")
 
 softwareWindows.addEventListener("click", function() {
     location.replace("https://mega.nz/file/RlpzBQaZ#LbOtpCfh3h0c5xqE8VF3saX0ud3NOTb0MNa8HMkpd6s")
@@ -7,4 +8,8 @@ softwareWindows.addEventListener("click", function() {
 
 softwareLinux.addEventListener("click", function() {
     location.replace("https://mega.nz/file/wowllKqL#T7WV0pfSqh9pjjeE3iOLJrH8ePXqcAWnuEEOcYmFxjE")
+})
+
+getStartedBtn.addEventListener("click", function() {
+    location.replace("https://automationnode.github.io/software/getStarted/index.html")
 })
