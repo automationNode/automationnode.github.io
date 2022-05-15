6e6991d2b160103ba311bed574408f004d5e8a59
automationNode <84304854+automationNode@users.noreply.github.com>
2022-05-15 00:07:20 -0600

    Delete pwa directory

--- pwa/js/index.js
+++
@@ -1,7 +0,0 @@
-var hardware = document.getElementById("hardware")
-var software = document.getElementById("software")
-var user = document.getElementById("user")
-
-hardware.addEventListener("click", function(){
-  location.replace("./hardware.html")
-})
